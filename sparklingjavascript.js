8ea9c21c13e782b6cdc1cd4be13fff534e434125
CodingLibrarianNinja <stephani.moore@gmail.com>
2016-11-07 15:14:27 -0500

    Second Version JavaScript file
    Uploaded wrong file-this is the right file

--- sparklingjavascript.js
+++ sparklingjavascript.js
@@ -1,9 +1,26 @@
 //INST630 Final Project-Stephani Moore/Rosie Yanosko
 //Sparkling Creations JavaScript Page
 
+var shoppingCart = []; //Global Variable to show empty shopping cart for the customer to put items in
+//icon at the top of each page so that the customer can click on it to see what they have in it so far.
+
 //Need code for having already made jewelry pieces pop out in smaller window
+//Photoviewer
+//Then they can add them to the cart if they want
 
 //Need code for having the person's custom ring photo to show up based
-//on form's input
+//on form's input Switch statement for different photos
 
-//Need code for adding items to the cart
+//Also, if/else statement for custom ring page
+//if the user likes their ring, then they will go to the order confirmation page.
+//else they will be returned to the form
+
+//Need code for adding items to the cart array-function
+//start with an empty array
+//Then as the customer adds items to the cart, then we will push that item to the array
+
+//Button as JavaScript will confirm the order and e-mail it to sparklingcreationsjewelry@gmail.com
+
+//storing variables for items that customer has added to cart and keeping them as the user continues to shop
+//global variables because we want them for all of the pages
+//until they exit the webpage completely
