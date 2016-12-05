437948fc59b1359078b7699ca952b059d71ad6ea
CodingLibrarianNinja <stephani.moore@gmail.com>
2016-12-05 16:21:21 -0500

    Edited out ringChoice functions
    I edited this file to remove all of the ringChoice functions as I moved them to a separate JS file to help the website run better.

--- sparklingjavascript.js
+++ sparklingjavascript.js
@@ -1,59 +1,7 @@
 //INST630 Final Project-Stephani Moore/Rosie Yanosko
 //Sparkling Creations JavaScript Page
+//JS file for form page
 
-//global variables
-//I want to declare variables using selections from the custom ring form
-
-
-var ringWire; 
-
-var crystalColor;
-
-
-
-//Need function for having the person's custom ring photo to show up based
-//on form's input Use if/else if statements
-function ringChoice(ringWire,crystalColor){
-	
-	ringWire = GetURLParameter("RingWire");
-	
-	crystalColor = GetURLParameter("CrystalColor");
-
-	if(ringWire === "Copper" && crystalColor === "Purple"){
-		document.getElementById("ringPhoto").src = "images/purplecopperwirering.jpg";
-	} else if(ringWire === "Copper" && crystalColor === "Red"){
-		document.getElementById("ringPhoto").src = "images/redcopperwirering.jpg";
-	} else if(ringWire === "Copper" && crystalColor === "Blue"){
-	    document.getElementById("ringPhoto").src = "images/bluecopperwirering.jpg";  
-    } else if(ringWire === "Copper" && crystalColor === "Black"){
-	    document.getElementById("ringPhoto").src = "images/blackcopperwirering.jpg";  
-	} else if(ringWire == "Copper" && crystalColor === "Pink"){
-	    document.getElementById("ringPhoto").src = "images/pinkcopperwirering.jpg";
-    } else if(ringWire === "Copper" && crystalColor === "Green"){
-	    document.getElementById("ringPhoto").src = "images/greencopperwirering.jpg";  
-    } else if(ringWire === "Copper" && crystalColor === "WhitePearl"){
-	    document.getElementById("ringPhoto").src = "images/pearlcopperwirering.jpg";
-	} else if(ringWire === "Silver" && crystalColor === "Purple"){    
-        document.getElementById("ringPhoto").src = "images/purplesilverwirering.jpg";  
-    } else if(ringWire === "Silver" && crystalColor === "Red"){ 
-	    document.getElementById("ringPhoto").src = "images/redsilverwirering.jpg"; 
-    } else if(ringWire === "Silver" && crystalColor === "Blue"){
-	    document.getElementById("ringPhoto").src = "images/bluesilverwirering.jpg";  
-    } else if(ringWire === "Silver" && crystalColor === "Black"){
-	    document.getElementById("ringPhoto").src = "images/blacksilverwirering.jpg";   
-    } else if(ringWire === "Silver" && crystalColor === "Pink"){  
-	    document.getElementById("ringPhoto").src = "images/pinksilverwirering.jpg"; 
-    } else if(ringWire === "Silver" && crystalColor === "Green"){  
-        document.getElementById("ringPhoto").src = "images/greensilverwirering.jpg";
-    } else if(ringWire === "Silver" && crystalColor === "WhitePearl"){
-		document.getElementById("ringPhoto").src = "images/pearlsilverwirering.jpg";
-	} else{
-		document.getElementById("ringPhoto").src = "images/customringphotolink.jpg";
-	}  
-		
-}
-
-ringChoice();
 
 
 //Function for pulling Parameters from form GET request
@@ -113,19 +61,8 @@ function userInfo(){
   
 	var email = GetURLParameter("Email");
 	 
-	  document.getElementById("Email").textContent = email;
+	  document.getElementById("Email").textContent = email; */
 	  
-	var ringSize = GetURLParameter("RingSize");
-	 
-	  document.getElementById("RingSize").textContent = ringSize;
-  
-	var ringWire = GetURLParameter("RingWire");
-		
-		document.getElementById("RingWire").textContent = ringWire;
-	
-	var crystalColor = GetURLParameter("CrystalColor");
-	
-		document.getElementById("CrystalColor").textContent = crystalColor;
   
 	var deliveryMethod = GetURLParameter("DeliveryMethod");
 	
@@ -137,20 +74,12 @@ function userInfo(){
   
 	var additionalInfo = GetURLParameter("AdditionalInfo");
 	 
-	  document.getElementById("AdditionalInfo").textContent = additionalInfo;
+	  document.getElementById("AdditionalInfo").textContent = additionalInfo; 
   
 	
 }
 
 userInfo();
 
-/*
-function validateForm() {
-    var zip = document.forms["customForm"] ["ZipCode"].value.toString();
-    if (zip.length > 5 || zip.length < 5) {
-        alert("Invalid Zip Code");
-        return false;
-    }
-}
-*/
+
 	
